86f6280ff7df0e2da13c61123e08c9d556c241d5
Eugene <77208278+Boft-ED@users.noreply.github.com>
2021-10-14 21:40:51 +0300

    new version site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
-    slidesPerView: 2,
+    slidesPerView: 1,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -8,4 +8,19 @@ var swiper = new Swiper(".mySwiper", {
     autoplay: {
         delay: 2500,
     },
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+        }
+    }
 });
+
+jQuery(document).ready(function ($) {
+    $('.burger, .overlay').click(function () {
+        $('.burger').toggleClass('clicked');
+        $('.overlay').toggleClass('show');
+        $('nav').toggleClass('show');
+        $('body').toggleClass('overflow');
+    });
+});
+
